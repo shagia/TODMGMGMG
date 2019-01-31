45435491a06c4fa446a359a4c5fe7973db8ed9e2
shagia <mrkraiola@gmail.com>
2019-01-30 20:24:00 -0800

    ??

---
+++ js/h.js
@@ -0,0 +1,56 @@
+var style1 = [
+     'border: 1px solid #3E0E02'
+    , 'color: green'
+    , "font-family: 'Comic Sans MS'"
+    , 'font-size: 33px'
+    , 'width: 2px'
+    , 'word-break: break-all'
+    , 'display: block'
+    , 'box-shadow: 0 1px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset'
+    , 'text-align: left'
+    , 'padding: 60px'
+    , 'line-height: 120px'
+    , 'background-image: url("https://t1.rbxcdn.com/88a18f22e2b6d64a180fdf17000ee20e")'
+    , 'background-size: 120px 120px'
+].join(';');
+var style2 = [
+     'border: 1px solid #3E0E02'
+    , 'color: red'
+    , 'font-size: 41px'
+    , "font-family: 'Times New Roman'"
+    , 'flex-direction: flex-end'
+    , 'background-image: url("https://i.imgur.com/7r32cSE.jpg")'
+    , 'box-shadow: 0 1px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset'
+    , 'padding: 10px'
+    , 'background-size: 520px 120px'
+    , 'background-position: center'
+    , 'text-align: right'
+    , 'font-weight: bold'
+].join(';');
+var style3 = [
+    'text-align: left'
+    , 'padding-right: 200px'
+    ,'font-size: 30px'
+].join(';');
+var space = [
+   'line-height: 50px'
+  , 'padding: 20px'
+  , 'color: grey'
+].join(';');
+var green = [
+  'background-color: green'
+  ,'color: white'
+  ,'padding-right: 300px'
+  , 'padding-top: 000px'
+].join(';');
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log("%c This a mf", style3);
+  console.log("%c i Still see ur shadows in my room....", space);
+  console.log('%c SHAGIA METRIC', style1);
+  console.log("%c and", green);
+  console.log('%c RICCO HARVER', style2);
+  console.log("%c joint, BITCH!!!!", style3);
+  console.log("%c best viewed on safare 11 Apple mac book pro 13' $2100 OBO | craigslist.com ", space);
+})
--- js/sketch.js
+++ js/sketch.js
@@ -1,69 +1,159 @@
 //this a SHAGIA METRIC x RICCO HARVER joint
 
 //Inits
+
 var fft
 var song
-var songItems = document.getElementsByClassName("songItem");
 
+
+//Player widget init
+var songItems = document.getElementsByClassName("songItem");
+var playerArtist = document.getElementById("metaArtist");
+var playerSong = document.getElementById("metaSong");
+var playControl = document.getElementById("playButton");
+
+
+
+
+
+function playerControl(event) {
+    if (event == false) {
+        playControl.querySelector('img').src = "img/ui/pause.png";
+    } else if (event == true) {
+        playControl.querySelector('img').src = "img/ui/play.png";
+    }
+
+}
+
+
+
+//Playback
+var audioPlaying = false
+
+function setup() {
+    //tracklist interaction
 function addEventListenerList(list, event, fn) {
     for (var i = 0, len = list.length; i < len; i++) {
         list[i].addEventListener(event, fn, false);
     }
 }
-//Playback
-var audioPlaying = false
+    var root = createCanvas(windowWidth, windowHeight);
+    root.parent('bg');
 
-function setup() {
-  var root = createCanvas(windowWidth, windowHeight);
-  root.parent('bg');
+    // default mode is radians
+    angleMode(DEGREES);
+    translate(width / 2, height / 2);
+    fft = new p5.FFT(0.5, 128);
 
-  // default mode is radians
-  angleMode(DEGREES);
-  translate(width/2, height/2);
+    //Main audio function
+    addEventListenerList(songItems, 'click', function() {
+      var context = new AudioContext();
 
-  
-  fft = new p5.FFT(0.5, 128);
-  
-  addEventListenerList(songItems, 'click', function() {
-  console.log(this.querySelector('audio').src);
+        if (audioPlaying === false) {
+            var curAudio = this.querySelector('audio').src;
+            var curArtist = this.querySelector('.listArtist').textContent;
+            var curPlay = this.querySelector('.listSong').textContent;
 
-  var curAudio = this.querySelector('audio').src;
-  song = createAudio(curAudio);
-  fft.setInput(song);
-  song.play();
+            song = createAudio(curAudio);
 
-});
+            song.crossOrigin = "Anonymous";
+            fft.setInput(song);
 
-  
-  
+            function touchStarted() {
+                context.resume()
+            }
+
+            function mousePressed() {
+                context.resume()
+            }
+            song.play();
+            audioPlaying = true
+            playerControl(false);
+            playerSong.innerHTML = curPlay;
+            playerArtist.innerHTML = curArtist;
+
+
+        } else if (audioPlaying === true) {
+
+            song.stop();
+            audioPlaying = false
+
+            var curAudio = this.querySelector('audio').src;
+            var curArtist = this.querySelector('.listArtist').textContent;
+            var curPlay = this.querySelector('.listSong').textContent;
+
+            song = createAudio(curAudio);
+
+
+
+            song.crossOrigin = "Anonymous";
+            fft.setInput(song);
+
+
+
+            function touchStarted() {
+                context.resume()
+            }
+
+            function mousePressed() {
+                context.resume()
+            }
+            song.play();
+            audioPlaying = true
+            playerSong.innerHTML = curPlay;
+            playerArtist.innerHTML = curArtist;
+
+
+        }
+    });
+
+
+
 };
+
+
+
+playControl.addEventListener('click', function() {
+    if (audioPlaying == false) {
+
+        song.play();
+        audioPlaying = true;
+        playerControl(false);
+
+
+    } else if (audioPlaying == true) {
+        song.pause();
+        audioPlaying = false;
+        playerControl(true);
+
+    }
+})
 
 function draw() {
 
-  background(0);
-  fill(216, 216, 216);
-  stroke(216, 216, 216, 100);
+    background(0);
+    fill(216, 216, 216);
+    stroke(216, 216, 216, 100);
 
-  //var spectrum = fft.analyze(128);
-  var spectrum = fft.analyze(256 * 2);
-  //console.log(spectrum);
+    //var spectrum = fft.analyze(128);
+    var spectrum = fft.analyze(256 * 2);
+    //console.log(spectrum);
 
- beginShape();
+    beginShape();
 
-  for (var i = 0; i < spectrum.length; i++) {
-  	var angle = map(i, 0, spectrum.length, 0, 360);
-  	//var amp = fft.analyze(256)[i];
-    var amp = spectrum[i] / 2;
-  	//var r = map(amp, 2, 512, 0, 500);
-    var r = map(amp + 20, 2, 512, 0, 500, amp);
-    var x = r * cos(angle);
-    var y = r * sin(angle);
-    vertex(x, y);
-    //triangle(0, 0, x, y);
-    //curveVertex(x, y);
-  }
+    for (var i = 0; i < spectrum.length; i++) {
+        var angle = map(i, 0, spectrum.length, 0, 360);
+        //var amp = fft.analyze(256)[i];
+        var amp = spectrum[i] / 2;
+        //var r = map(amp, 2, 512, 0, 500);
+        var r = map(amp + 20, 2, 512, 0, 500, amp);
+        var x = r * cos(angle);
+        var y = r * sin(angle);
+        vertex(x, y);
+        //triangle(0, 0, x, y);
+        //curveVertex(x, y);
+    }
 
-  endShape(CLOSE);
+    endShape(CLOSE);
 
 }
-
